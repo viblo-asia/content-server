76132f14f32d3f1bbe1980cdde2a6eab9460fcca
Nguyen Huu Kim <kimnguyen.ict@gmail.com>
2018-05-30 17:49:30 +0700

    Response target url in error page

--- src/routes/embed.js
+++ src/routes/embed.js
@@ -5,7 +5,12 @@ const resolve = require('path').resolve
 const router = express.Router()
 
 /** Response 404 page **/
-const sendErrorPage = (res) => res.status(404).sendFile(resolve(__dirname, '../views/404.html'))
+const sendErrorPage = (res, url) => res.status(404).send(
+    `<style>
+        .link {color:#5488c7;font-size:1rem;text-decoration:none}.link:hover{color:#33629c;text-decoration:underline}
+    </style>
+    <a class="link" href="${url}" target="_blank">${url}</a>`
+)
 
 /** The data variable is object, must contains "embedURL" or "document" **/
 const embedResponse = (res, data, viewPath) => {
@@ -14,7 +19,7 @@ const embedResponse = (res, data, viewPath) => {
     }
 
     return res.render(viewPath, data, (err, html) => {
-        return err ? sendErrorPage(res) : res.send(html)
+        return err ? sendErrorPage(res, data.url) : res.send(html)
     })
 }
 
@@ -27,12 +32,13 @@ const fetchSlideShare = (url) => {
 }
 
 router.get('/embed/slideshare', async (req, res) => {
-    if (!req.query.url) return sendErrorPage(res)
+    if (!req.query.url) return res.sendStatus(400)
 
-    const data = await fetchSlideShare(req.query.url).catch(() => sendErrorPage(res))
+    const url = req.query.url
+    const data = await fetchSlideShare(url).catch(() => sendErrorPage(res, url))
     const embedURL = `https://slideshare.net/slideshow/embed_code/${data.slideshow_id}`
 
-    return embedResponse(res, { embedURL, title: data.title })
+    return embedResponse(res, { embedURL, title: data.title, url })
 })
 
 module.exports = router
