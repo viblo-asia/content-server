22df36e09f5cc1f4898bac180276df9827d45b86
Nguyen Huu Kim <nguyen.huu.kim@framgia.com>
2018-11-20 14:42:30 +0700

    Fix embed jsfiddle: Connection refused

--- src/libs/viblo-embed/manual/parsers/jsfiddle.js
+++ src/libs/viblo-embed/manual/parsers/jsfiddle.js
@@ -1,15 +1,14 @@
 export default (code) => {
     let embedURL = code.replace(/^\/+|\/+$/gm, '')
         .replace(/(^http[s]?:\/\/(?:www\.)?jsfiddle.net\/)|(^jsfiddle.net\/)/, 'https://jsfiddle.net/')
-        .replace('/embed', '/embedded')
+        .replace(/\/embed(\/|\/.+)?$/, '/embedded$1')
 
     if (!embedURL.startsWith('https://jsfiddle.net/')) {
         embedURL = `https://jsfiddle.net/${embedURL}`
     }
 
-    // Support jsFiddle Url:
-    if (/^https:\/\/jsfiddle\.net(?:\/[^\s\/]+){2}$/.test(embedURL)) {
-        embedURL = `${embedURL}/embedded`
+    if (!embedURL.match(/(\/embedded)/)) {
+        embedURL = `${embedURL}/embedded/`
     }
 
     return {
