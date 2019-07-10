bb331b74ff758bebf81554a8406cc08db3aa973b
Nguyen Huu Kim <nguyen.huu.kim@framgia.com>
2019-07-10 14:51:57 +0700

    fix: embed user profile

--- src/libs/viblo-embed/manual/parsers/viblo.js
+++ src/libs/viblo-embed/manual/parsers/viblo.js
@@ -1,10 +1,11 @@
+import { APP_URL } from '../../../../env'
 
 export default (url) => {
     const regExp = /^http[s]?:\/\/?viblo.asia\/u\/([a-zA-Z0-9-]{0,20})/
     const match = url.match(regExp)
     if (!match) return
 
-    const embedURL = `embed/users/${match[1]}`
+    const embedURL = `${APP_URL}/embed/users/${match[1]}`
 
     return {
         src: embedURL,
