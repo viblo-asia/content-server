56171f5ca2a17905722cb51a30790d3300affb1c
Nguyen Huu Kim <nguyen.huu.kim@framgia.com>
2019-10-07 15:42:25 +0700

    vendor: bump viblo-sdk from v0.1.0-beta.19 to v0.1.0-beta.22

--- src/renderer.js
+++ src/renderer.js
@@ -1,7 +1,10 @@
 const createRenderer = require('viblo-sdk/markdown').createRenderer
 
 const md = createRenderer({
-    baseURL: process.env.APP_URL
+    baseURL: process.env.APP_URL,
+    maxSize: 500,
+    maxExpand: 100,
+    maxCharacter: 1000,
 })
 
 module.exports = md
